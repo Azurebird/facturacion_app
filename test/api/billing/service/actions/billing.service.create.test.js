fd800863fe4d31ed540bde1ade12a3ee934bccf9
Azurebird <white.camilo@gmail.com>
2019-01-20 22:35:51 -0500

    Packages for unit test

---
+++ test/api/billing/service/actions/billing.service.create.test.js
@@ -0,0 +1,7 @@
+import billingCreate 
+  from '../../../../../server/api/billing/service/actions/billing.service.create';
+
+describe('Tests for the billing creation', () => {
+  it('[ERROR] When the required parameters are not sent', () => {
+  });
+});
