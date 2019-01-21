ff90bad94b6c3c5a0ea0843322a3e02d4e7cc7d5
Azurebird <white.camilo@gmail.com>
2019-01-20 23:08:59 -0500

    async functions now working with babel

--- test/api/billing/service/actions/billing.service.create.test.js
+++ test/api/billing/service/actions/billing.service.create.test.js
@@ -1,7 +1,7 @@
-import billingCreate 
+import billingCreate
   from '../../../../../server/api/billing/service/actions/billing.service.create';
 
 describe('Tests for the billing creation', () => {
-  it('[ERROR] When the required parameters are not sent', () => {
+  it('[ERROR] When the required parameters are not sent', async () => {
   });
 });
